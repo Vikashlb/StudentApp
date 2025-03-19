6b5f9a647a813e2c217ee75123bec6ed21367c92
Vikashlb <lbvikashbabu10@gmail.com>
2025-03-19 16:39:36 +0530

    Added tests for delete and filterByName controller methods.

---
+++ student/src/test/java/com/app/student/controller/DeleteStudentTest.java
@@ -0,0 +1,61 @@
+package com.app.student.controller;
+
+import com.app.student.exception.StudentNotFoundException;
+import com.app.student.service.StudentServiceImpl;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+@WebMvcTest(controllers = StudentController.class)
+@ExtendWith(MockitoExtension.class)
+public class DeleteStudentTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockitoBean
+    private StudentServiceImpl studentService;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    private String universityNumber;
+
+    @BeforeEach
+    void init() {
+        universityNumber = "22MX228";
+    }
+
+    @Test
+    void ShouldReturnOk_WhenIdIsDeleted() throws Exception {
+        given(studentService.deleteStudent(ArgumentMatchers.any())).willReturn("Student Record Successfully Deleted");
+        ResultActions response = mockMvc.perform(delete("/deleteStudentById/{id}",universityNumber));
+
+        response.andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string("Student Record Successfully Deleted"));
+    }
+
+    @Test
+    void ShouldReturnNotFound_WhenIdIsNotAvailable() throws Exception {
+        given(studentService.deleteStudent(ArgumentMatchers.any()))
+                .willThrow(new StudentNotFoundException("Student Record Doesn't Exist!"));
+        ResultActions response = mockMvc.perform(delete("/deleteStudentById/{id}",universityNumber));
+
+        response.andExpect(MockMvcResultMatchers.status().isNotFound())
+                .andExpect(jsonPath("$.statusCode").value(404))
+                .andExpect(jsonPath("$.errorMessage").value("Student Record Doesn't Exist!"));
+    }
+}
---
+++ student/src/test/java/com/app/student/controller/GetStudentByNameTest.java
@@ -0,0 +1,78 @@
+package com.app.student.controller;
+
+import java.util.Arrays;
+import java.util.List;
+import com.app.student.entity.Student;
+import com.app.student.exception.StudentNotFoundException;
+import com.app.student.service.StudentServiceImpl;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.time.LocalDate;
+
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+@WebMvcTest(controllers = StudentController.class)
+@ExtendWith(MockitoExtension.class)
+public class GetStudentByNameTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockitoBean
+    private StudentServiceImpl studentService;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    private String firstName;
+    private List<Student> students;
+
+    @BeforeEach
+    void init() {
+        firstName = "Vik";
+        students = Arrays.asList(
+                new Student("22MX228", "Vikash", "Bojarajan",
+                        "Computer Applications", 84.30, LocalDate.parse("2002-06-10")),
+                new Student("22MX221", "Vikram", "Kumar",
+                        "Computer Applications", 90.30, LocalDate.parse("2001-08-21"))
+        );
+    }
+
+    @Test
+    void ShouldReturnOk_WhenDataIsAvailable() throws Exception {
+        given(studentService.getStudentsByFirstName(ArgumentMatchers.any())).willReturn(students);
+        ResultActions response = mockMvc.perform(get("/getStudentByName/{name}",firstName)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(students)));
+
+        response.andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(jsonPath("$[0].firstName").value("Vikash"))
+                .andExpect(jsonPath("$[1].firstName").value("Vikram"));
+    }
+
+    @Test
+    void ShouldReturnNotFound_WhenDataIsNotAvailable() throws Exception {
+        given(studentService.getStudentsByFirstName(ArgumentMatchers.any()))
+                .willThrow(new StudentNotFoundException("Student Record With Name : '"+ firstName +"' Not Found!"));
+        ResultActions response = mockMvc.perform(get("/getStudentByName/{name}",firstName)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(students)));
+
+        response.andExpect(MockMvcResultMatchers.status().isNotFound())
+                .andExpect(jsonPath("$.statusCode").value(404))
+                .andExpect(jsonPath("$.errorMessage").value("Student Record With Name : '"+ firstName +"' Not Found!"));
+    }
+}
