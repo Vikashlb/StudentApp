4cd8da3f9573b6e50ceda7a30fd65e12711f6da8
Vikashlb <lbvikashbabu10@gmail.com>
2025-03-19 10:24:40 +0530

    Added tests for add and update methods.

---
+++ student/src/test/java/com/app/student/service/AddStudentTest.java
@@ -0,0 +1,48 @@
+package com.app.student.service;
+
+import com.app.student.entity.Student;
+import com.app.student.exception.DuplicateRecordException;
+import com.app.student.repository.StudentRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDate;
+
+@SpringBootTest
+@ExtendWith(MockitoExtension.class)
+public class AddStudentTest {
+    @Mock
+    private StudentRepository studentRepository;
+
+    @InjectMocks
+    private StudentServiceImpl studentService;
+
+    @Test
+    void ShouldReturnStudent_WhenStudentRecordDoesNotExist() {
+        Student student = new Student("22MX228", "Sanjay", "Krishna",
+                "Computer Science", 84.30, LocalDate.parse("2002-06-10"));
+
+        Mockito.when(studentRepository.existsById("22MX228")).thenReturn(false);
+
+        Student result = studentService.addStudent(student);
+
+        Assertions.assertEquals(student, result);
+        Mockito.verify(studentRepository, Mockito.times(1)).save(student);
+    }
+
+    @Test
+    void ShouldThrowException_WhenRecordExists() {
+        Student student = new Student("22MX228", "Sanjay", "Krishna",
+                "Computer Science", 84.30, LocalDate.parse("2002-06-10"));
+        Mockito.when(studentRepository.existsById("22MX228")).thenReturn(true);
+
+        Assertions.assertThrows(DuplicateRecordException.class, () -> studentService.addStudent(student));
+        Mockito.verify(studentRepository, Mockito.never()).save(student);
+    }
+}
---
+++ student/src/test/java/com/app/student/service/UpdateStudentTest.java
@@ -0,0 +1,50 @@
+package com.app.student.service;
+
+import com.app.student.entity.Student;
+import com.app.student.exception.StudentNotFoundException;
+import com.app.student.repository.StudentRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDate;
+
+@SpringBootTest
+@ExtendWith(MockitoExtension.class)
+public class UpdateStudentTest {
+    @Mock
+    private StudentRepository studentRepository;
+
+    @InjectMocks
+    private StudentServiceImpl studentService;
+
+    @Test
+    void ShouldReturnStudent_WhenIdExists() {
+        Student student = new Student("22MX228", "Sanjay", "Krishna",
+                "Computer Science", 84.30, LocalDate.parse("2002-06-10"));
+        Mockito.when(studentRepository.existsById("22MX228")).thenReturn(true);
+        Mockito.when(studentRepository.save(student)).thenReturn(student);
+
+        Student result = studentService.updateStudent(student,"22MX228");
+
+        Assertions.assertNotNull(result);
+        Assertions.assertEquals(student, result);
+
+        Mockito.verify(studentRepository).existsById("22MX228");
+        Mockito.verify(studentRepository, Mockito.times(1)).save(student);
+    }
+
+    @Test
+    void ShouldThrowException_WhenIdDoesNotExist() {
+        Student student = new Student("22MX228", "Sanjay", "Krishna",
+                "Computer Science", 84.30, LocalDate.parse("2002-06-10"));
+        Mockito.when(studentRepository.existsById("22MX228")).thenReturn(false);
+        Assertions.assertThrows(StudentNotFoundException.class, () -> studentService.updateStudent(student, "22MX228"));
+        Mockito.verify(studentRepository, Mockito.never()).save(student);
+    }
+}
