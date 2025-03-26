da086e581f4f2fc52ce25bfcdfe3708ffa9ed18d
Vikashlb <lbvikashbabu10@gmail.com>
2025-03-26 11:54:40 +0530

    Added table and column names!

--- student/src/main/java/com/app/student/entity/Student.java
+++ student/src/main/java/com/app/student/entity/Student.java
@@ -1,11 +1,14 @@
 package com.app.student.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.*;
 import java.time.LocalDate;
 
 @Entity
+@Table(name = "student")
 public class Student {
 
   @Id
@@ -13,28 +16,34 @@ public class Student {
   @Pattern(
       regexp = "^[A-Za-z0-9]+$",
       message = "University number must contain only alphanumeric characters!")
+  @Column(name = "university_number")
   private String universityNumber;
 
   @NotBlank(message = "First name cannot be empty!")
   @Pattern(
       regexp = "^[a-zA-Z]+$",
       message = "Student's firstname must contain only alphabets with no blank spaces")
+  @Column(name = "first_name")
   private String firstName;
 
   @NotBlank(message = "Last name cannot be empty!")
   @Pattern(
       regexp = "^[a-zA-Z]+$",
       message = "Student's lastname must contain only alphabets with no blank spaces")
+  @Column(name = "last_name")
   private String lastName;
 
   @NotBlank(message = "Department name cannot be empty!")
+  @Column(name = "department_name")
   private String departmentName;
 
   @Min(value = 1, message = "Percentage must be greater than zero!")
+  @Column(name = "percentage")
   private double percentage;
 
   @NotNull(message = "Date of Birth cannot be empty!")
   @PastOrPresent(message = "Date of Birth must only be in past or present!")
+  @Column(name = "date_of_birth")
   private LocalDate dateOfBirth;
 
   // DefaultConstructor
